66edefc4556b92c610f3445251b896201801ae4c
alexcalvo135 <72521421+alexcalvo135@users.noreply.github.com>
2022-11-22 02:59:51 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ const port = 2000
 const dbcon = db.connect()
 
 app.use(cors())
+//parsea contentType urlencoded(como se pasa el body)
+app.use(express.urlencoded({extended:true}))
 //servir estaticamente la carpeta public
 app.use(express.static("public"))
 
@@ -38,13 +40,35 @@ app.get('/api/excursiones/:id', (req, res) => {
 })
 
 app.get('/api/excursiones', (req, res) => {
-  const query = `SELECT * FROM excursiones`
+  const mes = req.query['mes']
+  const destino = req.query['destino']
+  //SIEMPRE HAY CONDICION TRUE
+  var query = `SELECT * FROM excursiones WHERE 1=1 `
+  if(mes){
+    const now = new Date()
+    if(mes==="noviembre"){
+      query+=` AND fecha_inicio BETWEEN CAST('${now.getFullYear()}-11-1' AS DATE) AND CAST('${now.getFullYear()}-11-30' AS DATE) `
+    }else if(mes==="septiembre"){
+      query+=` AND fecha_inicio BETWEEN CAST('${now.getFullYear()}-09-1' AS DATE) AND CAST('${now.getFullYear()}-09-30' AS DATE) `
+
+    }else if(mes==="octubre"){
+      query+=` AND fecha_inicio BETWEEN CAST('${now.getFullYear()}-10-1' AS DATE) AND CAST('${now.getFullYear()}-10-31' AS DATE) `
+
+    }else if(mes==="diciembre"){
+      query+=` AND fecha_inicio BETWEEN CAST('${now.getFullYear()}-12-1' AS DATE) AND CAST('${now.getFullYear()}-12-31' AS DATE) `
+
+    }
+  }
+  if(destino){    
+    query+= `AND destino = "${destino}"`
+  }
   dbcon.query(query, function (err, result, fields) {
     if (err) throw err
     
     res.json(result)
   });
 })
+
 
 //BLOG
 
@@ -70,6 +94,39 @@ app.get('/api/blog', (req, res) => {
   });
 })
 
+//CONTACTO
+
+var nodemailer = require('nodemailer');
+const bodyParser = require("body-parser")
+
+app.post('/api/contacto', (req, res) => {
+
+  const {nombre, email, asunto, mensaje} = req.body
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    //autenticacion gmail
+    auth: {
+      user: 'acalvoconpelo@gmail.com',
+      pass: 'eoowrypcrwsvgeki'
+    }
+  });
+
+  var mailOptions = {
+    from: 'acalvoconpelo@gmail.com',
+    to: 'acalvoconpelo@gmail.com',
+    subject: asunto,
+    text: mensaje
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+})
 //servir estaticamente el frontend
 app.use("/js",express.static("../frontend/js"))
 app.use("/css",express.static("../frontend/css"))
