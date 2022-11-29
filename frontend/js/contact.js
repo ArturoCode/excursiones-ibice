6d790e4ac413f40d41bc10c796a70754215de0c2
Arturo Urbanos Vara <90784004+ArturoCode@users.noreply.github.com>
2022-11-29 19:36:24 +0100

    FEAT: Upload changes

---
+++ frontend/js/contact.js
@@ -0,0 +1,92 @@
+
+async function enviarMailContacto(event){
+    
+    const body = {
+       nombre: event.target.nombre.value,
+       email: event.target.email.value, 
+       asunto: event.target.asunto.value, 
+       mensaje: event.target.mensaje.value,  
+    }
+
+    var url = `${config.urlBackend}/contacto`;
+
+    const res = await fetch(url,{method:"post", body:JSON.stringify(body), headers:{"Content-Type":"application/json"}}) 
+
+    if(res.status === 200) document.getElementById("mensaje-ok").style.display = "block"
+
+    return false
+}
+
+const form = document.getElementById('form');
+const nombreCompleto = document.getElementById('nombre');
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+
+form.addEventListener('submit', e => {
+
+	e.preventDefault();	
+	
+    var isError = checkInputs();
+
+    if(!isError) enviarMailContacto(e);    
+});
+
+function checkInputs() {
+		
+    var error = false;
+	const nameValue = nombreCompleto.value.trim();
+	const emailValue = email.value.trim();
+	const asuntoValue = asunto.value.trim();
+	const messageValue = mensaje.value.trim();
+	
+	if(nameValue === '') {
+		setErrorFor(nombreCompleto, 'El nombre no puede estar en blanco');
+        error = true
+	} else {
+		setSuccessFor(nombreCompleto);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'El email no puede estar en blanco');
+        error = true
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Email no válido');
+        error = true
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(asuntoValue === '') {
+		setErrorFor(asunto, 'Tiene que tener un asunto');
+        error = true
+	} else {
+		setSuccessFor(asunto);
+	}
+	
+	if(messageValue === '') {
+		setErrorFor(mensaje, 'Tienes que escribir un mensaje');
+        error = true
+	} else {
+		setSuccessFor(mensaje);
+	}
+    return error
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control-input error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control-input success';
+}
+
+
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
---
+++ frontend/js/header.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  usuarioLoggeado();
+});
+function usuarioLoggeado() {
+  var user = localStorage.getItem("user");
+
+  if (user) {   
+    user = JSON.parse(user);
+    document.getElementById("header-nombre-usuario").style.display = "block";
+    document.getElementById("iniciar-sesion").style.display = "none";
+    document.getElementById("barra1").style.display = "none";
+    document.getElementById("barra2").style.display = "none";
+    document.getElementById("registrarse").style.display = "none";
+    document.getElementById("perfil").style.display = "block";
+    document.getElementById("barra3").style.display = "block";
+    document.getElementById("cerrar-sesion").style.display = "block";
+    document.getElementById("header-nombre-usuario").innerHTML = user.nombre;    
+  }
+}
+
+function cerrarSesion() {
+    
+  var url = `${config.urlBackend}/desconectarse`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log("aa")
+      localStorage.removeItem("user");
+      window.location.href = `${config.url}/index.html`
+    });
+
+}
