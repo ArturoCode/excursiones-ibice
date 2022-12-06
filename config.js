b05bcd92f150d6e6348aa1b773e04e99d201652c
Arturo Urbanos Vara <90784004+ArturoCode@users.noreply.github.com>
2022-12-06 15:05:47 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = process.env.PORT || 2000;
-
-export const db_host = process.env.DB_HOST || 'localhost'
-export const db_user = process.env.DB_USER || 'root'
-export const db_password = process.env.DB_PASSWORD || ''
-export const db_name = process.env.DB_NAME || 'excursiones-ibice'
-export const db_port = process.env.DB_PORT || '3306'
