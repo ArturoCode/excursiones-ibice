826eefd989c6ed771b51c38d9e2afd4a25621ad6
Arturo Urbanos Vara <90784004+ArturoCode@users.noreply.github.com>
2022-12-06 15:06:51 +0100

    TEST: Variables changes

--- backend/sqlconnector.js
+++ backend/sqlconnector.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
+
+const db_host = process.env.DB_HOST || "localhost"
+const db_user = process.env.DB_USER || "root"
+const db_password = process.env.DB_PASSWORD || ""
+const db_name = process.env.DB_NAME || "excursiones-ibice"
+
 function connect(){
     var connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "excursiones-ibice"
+        host: db_host,
+        user: db_user,
+        password: db_password,
+        database: db_name
     });  
     connection.connect(function(err) {
         if (err) throw err;
