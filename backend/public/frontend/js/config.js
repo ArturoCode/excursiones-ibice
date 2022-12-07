0e14e76a1715bfd8720fad38deb1dc7e521a03eb
Arturo Urbanos Vara <90784004+ArturoCode@users.noreply.github.com>
2022-12-07 16:48:35 +0100

    Delete backend directory

--- backend/public/frontend/js/config.js
+++
@@ -1,7 +0,0 @@
-//variables globales de configuracion
-
-var config = {
-    urlBackend: "https://proyecto-final-production-36eb.up.railway.app/api",
-    url: "https://proyecto-final-production-36eb.up.railway.app"
-}
-
