a8e4f26020d2bd63d9df5a6e249f29fb1ee3b69e
Arturo Urbanos Vara <90784004+ArturoCode@users.noreply.github.com>
2022-12-07 17:17:14 +0100

    FEAT: Railway implementation

--- app/public/frontend/js/config.js
+++ app/public/frontend/js/config.js
@@ -2,6 +2,6 @@
 
 var config = {
     urlBackend: "https://excursiones-ibice.up.railway.app/api",
-    url: "https://proyecto-final-production-36eb.up.railway.app"
+    url: "https://excursiones-ibice.up.railway.app"
 }
 
